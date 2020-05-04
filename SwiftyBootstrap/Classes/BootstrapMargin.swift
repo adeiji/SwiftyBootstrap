8f7e5e2b4cfad54a8f9f450c7ada1422e1f8811e
Adebayo Ijidakinro <adeiji@yahoo.com>
2020-05-04 16:42:44 +0800

    Added public init for BootstrapMargin struct

--- SwiftyBootstrap/Classes/BootstrapMargin.swift
+++ SwiftyBootstrap/Classes/BootstrapMargin.swift
@@ -7,9 +7,20 @@
 
 import Foundation
 
+/**
+ Margin that it used specifically within the GRBootstrapElement class. This is the outer margin of the object
+ */
 public struct BootstrapMargin {
-    var left:CGFloat? = 20
-    var top:CGFloat? = 20
-    var right:CGFloat? = 20
-    var bottom:CGFloat? = 20
+    let left:CGFloat
+    let top:CGFloat
+    let right:CGFloat
+    let bottom:CGFloat
+    
+    public init (left:CGFloat?, top:CGFloat?, right:CGFloat?, bottom:CGFloat?) {
+        self.left = left ?? 20
+        self.top = top ?? 20
+        self.right = right ?? 20
+        self.bottom = bottom ?? 20
+    }
 }
+
